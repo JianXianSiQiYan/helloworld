82e9ac1187d52659a1930ad2c8c1e51b42a0cfc0
JianXianSiQiYan <790812765@qq.com>
2022-12-05 07:37:41 +0800

    helloworld 2022-12-05

---
+++ helloworld.cpp
@@ -0,0 +1,21 @@
+﻿#include <gtest/gtest.h>
+#include <mockcpp/mockcpp.hpp>
+#include <iostream>
+int func()
+{
+    return 10;
+}
+
+int func1()
+{
+    return 100;
+}
+
+TEST(mockercpp, helloworld) {
+    std::cout << func() << std::endl;
+    MOCKER(func)
+        .stubs()
+        .will(invoke(func1));
+    std::cout << func() << std::endl;
+    EXPECT_TRUE(true);
+}
